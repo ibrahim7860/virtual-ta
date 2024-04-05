e2ca4f8ceb10fed11e505bb55457c942a9edae98
qasimb03 <4qasim@gmail.com>
2024-04-05 03:35:10 -0500

    Chat history

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import Login from "./Login";
 import Register from "./Register";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ForgotPassword from "./ForgotPassword";
 import Navbar from "./Navbar";
 import Homepage from "./HomePage";
@@ -9,17 +9,17 @@ import EnrollMFA from "../authentication/EnrollMFA";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="sign-in" element={<Login />} />
-              <Route path="forgot-password" element={<ForgotPassword />} />
-              <Route path="create-account" element={<Register />} />
-              <Route path="chat-page" element={<ChatbotPage />} />
-              <Route path="enroll-mfa" element={<EnrollMFA />} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="sign-in" element={<Login />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="create-account" element={<Register />} />
+        <Route path="chat-page" element={<ChatbotPage />} />
+        <Route path="enroll-mfa" element={<EnrollMFA />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
